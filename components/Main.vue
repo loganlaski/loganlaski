8ffa77de713c2834c3fdaeace51c4cc22204cfeb
logan laski <laskilogan@gmail.com>
2024-01-08 10:14:38 -0600

    [overall update]

--- components/Main.vue
+++ components/Main.vue
@@ -1,29 +1,26 @@
 <template>
     <div id="component" class="p-5">
         <div class="block">
-            <div>
-                <h1>
-                    Logan Laski
-                </h1>
+            <h1>
+                Logan Laski
+            </h1>
 
-                <h1>
-                    Creative technologist based in Madison, WI.
-                </h1>
+            <h1>
+                Creative technologist based in Madison, WI.
+            </h1>
 
-                <h1>
-                    Focused on building modern digital products.
-                </h1>
+            <h1>
+                Focused on building modern digital products.
+            </h1>
 
-                <h1 class="mt-4">
-                    Currently working on:
-                </h1>
+            <h1 class="mt-4">
+                Currently working on:
+            </h1>
 
-                <div v-for="link in projectLinks">
-                    <!-- <img :src="" class="" /> -->
-                    <a :href="link.href" target="_blank" class="hover:opacity-75">
-                        {{ link.item }}
-                    </a>
-                </div>
+            <div v-for="link in projectLinks">
+                <a :href="link.href" target="_blank" class="hover:opacity-75">
+                    {{ link.item }}
+                </a>
             </div>
 
             <ul class="block mt-4">
@@ -45,19 +42,21 @@
 const projectLinks = [
     {
         id: 0,
-        // iconUrl: ,
         item: 'lrl-design',
         href: 'https://lrl.design',
     },
     {
         id: 1,
-        // iconUrl: ,
         item: 'glassleaf-app',
         href: 'https://glassleaf.app',
     },
     {
         id: 2,
-        // iconUrl: ,
+        item: 'orderblock-app',
+        href: 'https://orderblock.app',
+    },
+    {
+        id: 3,
         item: 'freedays-studio',
         href: 'https://freedays.studio',
     },
@@ -93,8 +92,8 @@ const contactLinks = [
 
 <style>
 * {
-    font-size: 0.95rem;
-    line-height: 125%;
+    font-size: 1rem;
+    line-height: 120%;
 }
 h1, p {
     pointer-events: none;
